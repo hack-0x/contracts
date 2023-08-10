76d7a9266aa2a94933d242abb67b10bcc67b23ef
Liron Achdut <liron.achdut@gmail.com>
2023-08-10 16:45:51 +0300

    all foundry files

---
+++ script/Deploy.s.sol
@@ -0,0 +1,37 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+// import "../contracts/YourContract.sol";
+import "../contracts/Project.sol";
+import "../contracts/UserRegistry.sol";
+import "./DeployHelpers.s.sol";
+
+contract DeployScript is ScaffoldETHDeploy {
+    function run() external {
+        uint256 deployerPrivateKey = setupLocalhostEnv();
+        vm.startBroadcast(deployerPrivateKey);
+
+        UserRegistry userRegistry = new UserRegistry(
+            0x0f4322635b17775CF7F6A42537Ccc11874342B55
+        );
+
+        // UsersRegistry userRegistry = new UsersRegistry(address(0));
+
+        Project project = new Project(
+            0x0f4322635b17775CF7F6A42537Ccc11874342B55,
+            address(userRegistry)
+        );
+
+        vm.stopBroadcast();
+        /**
+         * This function generates the file containing the contracts Abi definitions.
+         * These definitions are used to derive the types needed in the custom scaffold-eth hooks, for example.
+         * This function should be called last.
+         */
+        exportDeployments();
+        // If your chain is not present in foundry's stdChain, then you need to call function with chainName:
+        // exportDeployments("chiado")
+    }
+
+    function test() public {}
+}
