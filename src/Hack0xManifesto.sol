6e18822673dcbaba68d7a03ca22cdbea32bb220b
ethan <contact@ethan-rouimi.com>
2023-08-12 23:36:51 +0100

    resolving PR

--- src/Hack0xManifesto.sol
+++ src/Hack0xManifesto.sol
@@ -1,19 +1,19 @@
-// SPDX-License-Identifier: MIT 
-pragma solidity 0.8.21;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Hack0xManifesto is ERC721 {
+    string immutable MANIFESTO =
+        "hack the status quo - a manifesto.\n\n" +
+            "the membership is 10 op. it's for life.\n" +
+            "you get an on-chain attestation, and you have full access to the collective. " +
+            "meaning: see all projects, create new projects, find contributors/partners-in-crime, get funds, support public goods.\n" +
+            "break things, change things. give explanations to no one.\n" +
+            "this is a manifesto, not a 'tos'\n" +
+            "by signing, you share these collective values. you won't be giving up your data, your privacy, your identity, or whatever.\n" +
+            "this is your self-sovereign choice, and it has a date. today.";
 
-    string immutable MANIFESTO = "hack the status quo - a manifesto.\n\n" +
-    "the membership is 10 op. it's for life.\n" +
-    "you get an on-chain attestation, and you have full access to the collective. " +
-    "meaning: see all projects, create new projects, find contributors/partners-in-crime, get funds, support public goods.\n" +
-    "break things, change things. give explanations to no one.\n" +
-    "this is a manifesto, not a 'tos'\n" +
-    "by signing, you share these collective values. you won't be giving up your data, your privacy, your identity, or whatever.\n" +
-    "this is your self-sovereign choice, and it has a date. today.";
-    
     constructor() ERC721("Hack0xManifesto", "H0xM") {}
 
     function sign() public payable {
@@ -24,7 +24,11 @@ contract Hack0xManifesto is ERC721 {
         return "https://hack0x.io/manifesto/"; // TODO - update
     }
 
-    function transferFrom(address from, address to, uint256 tokenId) public override {
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public override {
         revert("Hack0xManifesto is not transferable");
     }
 }
